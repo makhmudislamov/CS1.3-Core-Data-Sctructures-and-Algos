6eaa5de1afcfe8ecdaa9057e616929007d9d81a4
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-04-23 18:03:49 -0700

    finished Set class skeleton

--- source/set.py
+++ source/set.py
@@ -5,6 +5,32 @@ class Set(object):
     def __init__(self, elements=None):
         self.size = 0
 
-    
+    def contains(self, element):
+        """Returns a boolean indicating whether element is in this set"""
+        pass
 
+    def add(self, element):
+        """Adds element to this set, if not present already"""
+        pass
+    
+    def remove(self, element):
+        """Removes element from this set, if present, or else raise KeyError"""
+        pass
+
+    def union(self, other_set):
+        """Returns a new set that is the union of this set and other_set"""
+        pass
+
+    def intersection(self, other_set):
+        """Returns a new set that is the intersection of this set and other_set"""
+        pass
+    
+    def difference(self, other_set):
+        """Returns a new set that is the difference of this set and other_set"""
+        pass
+    
+    def is_subset(self, other_set):
+        """Returns a boolean indicating whether other_set is a subset of this set"""
+        pass
+    
     
