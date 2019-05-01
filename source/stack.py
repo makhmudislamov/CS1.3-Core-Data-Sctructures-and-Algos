6fbe10a2a06f3968cbcf447d085720005e71a3c3
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-05-01 06:10:15 -0700

    implemented ArrayStack methods

--- source/stack.py
+++ source/stack.py
@@ -58,9 +58,9 @@ class LinkedStack(object):
         if self.list.head is None:
             raise ValueError('The stack is empty')
         else:
-            current_tail = self.list.tail.data
+            top_data = self.list.tail.data
             self.list.size -= 1
-            return self.list.delete(current_tail)
+            self.list.delete(top_data)
 
 # Implement ArrayStack below, then change the assignment at the bottom
 # to use this Stack implementation to verify it passes all tests
@@ -81,29 +81,46 @@ class ArrayStack(object):
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
         # TODO: Check if empty
+        if self.list[0] is None:
+            return True
+        else:
+            return False
 
     def length(self):
         """Return the number of items in this stack."""
         # TODO: Count number of items
+        return len(self.list)
 
     def push(self, item):
         """Insert the given item on the top of this stack.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(1) – Why? >>> we can access the end of the array without traversing it"""
         # TODO: Insert given item
+        self.list.append(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
         # TODO: Return top item, if any
+        if self.list is None:
+            return None
+        else:
+            last_index = len(self.list) - 1
+            return self.list[last_index]
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(n) – Why? >>> removing last item is O(1) but in order to know what is new last item we have to traverse"""
         # TODO: Remove and return top item, if any
+        
+        if self.list[0] is None:
+            raise ValueError('The Stack is empty')
+        else:
+            self.list.pop(-1)
+            return self.list[-1]
 
 
 # Implement LinkedStack and ArrayStack above, then change the assignment below
 # to use each of your Stack implementations to verify they each pass all tests
-Stack = LinkedStack
-# Stack = ArrayStack
+# Stack = LinkedStack
+Stack = ArrayStack
