25b776e596e2aa9bbfc92659074c4405f787569e
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-05-02 18:41:54 -0700

    optimization via contains()

--- source/my_set.py
+++ source/my_set.py
@@ -15,9 +15,8 @@ class Set(object):
     def contains(self, element):
         """Returns a boolean indicating whether element is in this set
         Time Complexity: O(1) >> hashtable lookup is a const operation on average case
-        Space Complexity: O(1) >> accessing data
+        Space Complexity: O(n) >> n is a bucket size.
         """
-        #TODO: write test
         if not self.hashtable:
             raise ValueError('Empty Hashtable')
         else:
@@ -29,15 +28,15 @@ class Set(object):
         Space Complexity: O(1) >> one new space is created for new element
         """
         if self.contains(element) is True:
-            return element
+            return 
         else:
             self.hashtable.set(element, None)
             self.size += 1
     
     def remove(self, element):
         """Removes element from this set, if present, or else raise KeyError
-        Time Complexity: O(1) >> delete method of hastable is const time
-        Space Complexity: O(1) >> removing the element is const time, no need to traverse
+        Time Complexity: O(n) >> delete method of hastable is const time
+        Space Complexity: O(n) >> 
         """
         if self.contains(element) is False:
             raise KeyError('Element does not exist')
@@ -47,8 +46,8 @@ class Set(object):
 
     def union(self, other_set):
         """Returns a new set that is the union of this set and other_set
-        Time Complexity: O(2n) >> O(n) >> travaersing through the hashtables to collect the elements 
-        Space Complexity: O(n) >> creating space for new each element one by one
+        Time Complexity: O(2n) >> O(n) >> travaersing through the hashtables to collect the elements. n + m  
+        Space Complexity: O(n) >> creating space for new each element one by one. n + m  
         """
         united_set = Set()
         # adding elements of self to united_set
@@ -77,7 +76,7 @@ class Set(object):
             small_set = self
 
         for element in small_set.hashtable.keys():
-            if element in big_set.hashtable.keys():
+            if big_set.hashtable.contains(element) is True:
                 inter_set.add(element)
         
         return inter_set
@@ -86,20 +85,16 @@ class Set(object):
         """Returns a new set that is the difference of this set and other_set
         Time Complexity: O(n) >> travaersing through the hashtable to collect and compare the elements 
         Space Complexity: O(n) >> creating space for new each element one by one
-        """
+        """ 
         differ_set = Set()
-        # smaller set has to be iterated and its elements should be compared to larger set
-        if self.size > other_set.size:
-            big_set = self
-            small_set = other_set
-        else:
-            big_set = other_set
-            small_set = self
-
-        for element in small_set.hashtable.keys():
-            if element not in big_set.hashtable.keys():
+        for element in self.hashtable.keys():  
+            if not other_set.hashtable.contains(element):
                 differ_set.add(element)
         
+        for element in other_set.hashtable.keys():
+            if not self.hashtable.contains(element):
+                differ_set.add(element)
+
         return differ_set
         
     
@@ -114,7 +109,7 @@ class Set(object):
             small_set = self
 
         for element in small_set.hashtable.keys():
-            if element in big_set.hashtable.keys():
+            if big_set.hashtable.contains(element) is True:
                 return True
             else:
                 return False
